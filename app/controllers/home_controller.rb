4c063a1649ea97351183d168590fa951dd16c158
Himanshu Bainwala <hbainwala@gmail.com>
2021-02-11 12:08:10 -0600

    HW3

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def first
+    numbers = Array['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    suits = Array['CLUBS', 'SPADES', 'HEARTS', 'DIAMONDS']
+    @value = Array[numbers[rand(13)], numbers[rand(13)], numbers[rand(13)], numbers[rand(13)], numbers[rand(13)]]
+    @suit = Array[suits[rand(4)], suits[rand(4)], suits[rand(4)], suits[rand(4)], suits[rand(4)]]
   end
 
   def second
