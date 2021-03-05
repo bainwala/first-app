0ca0a9ec5ed42576d5d65ce163d888650fb5f5d3
Himanshu Bainwala <hbainwala@gmail.com>
2021-03-04 22:56:05 -0600

    Default database values

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 
 class HomeController < ApplicationController
   def first
+    count = 0
     res = RestClient.get 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
     data = JSON.parse res
     card = RestClient.get "https://deckofcardsapi.com/api/deck/#{data["deck_id"]}/draw/?count=5"
@@ -25,4 +26,12 @@ class HomeController < ApplicationController
     @user = UserDetail.create(params.require(:user_detail).permit(:name, :username))
     redirect_to root_path
   end
+
+  # def set_defaults
+  #   @user.current_tokens ||= 0
+  #   @user.self.wins ||=0
+  #   @user.losses ||=0
+  # end
+
+  #after_action :set_defaults
 end
--- app/models/user_detail.rb
+++ app/models/user_detail.rb
@@ -1,3 +1,10 @@
 class UserDetail < ApplicationRecord
-    
+    after_initialize :init
+
+    def init
+      self.username ||= "John Doe"
+      self.wins  ||= 0
+      self.losses ||=0
+      self.current_tokens ||=100          
+    end
 end
